a37e72f461b07b9ff06b5bb3a3299a2ace639426
toshioyasunaka <toshioyasunaka@hotmail.com>
2022-10-24 18:55:53 -0300

    finished banners

--- assets/js/modules/banner-slider.js
+++ assets/js/modules/banner-slider.js
@@ -1,9 +1,16 @@
 const sliderItems = document.querySelectorAll('[data-banner="item"]');
 const slider = document.querySelector('[data-banner="slider"]');
+const btnNext = document.querySelector('[data-banner="btn-next"]');
+const btnPrevious = document.querySelector('[data-banner="btn-previous"]');
+const btnControls = document.querySelectorAll('[data-banner="btn-control"]');
+const imgTitles = document.querySelectorAll('[data-banner="img-title"]');
+
 const state = {
     mouseDownPosition: 0,
     lastTranslatePosition: 0,
     currentSliderPosition: 0,
+    movementPosition: 0,
+    currentSlideIndex: 0,
 }
 
 function translateSlide(position) {
@@ -11,45 +18,122 @@ function translateSlide(position) {
     slider.style.transform = `translateX(${position}px)`
 }
 
+function getCenterPosition(index) {
+    const slide = sliderItems[index];
+    const margin = (window.innerWidth - slide.offsetWidth) / 2;
+    const centerPosition = margin - (slide.offsetWidth * index);
+    return centerPosition;
+}
+
+function forwardSlide() {
+    if (state.currentSlideIndex < sliderItems.length - 1) {
+        setVisibleSlide(state.currentSlideIndex + 1);
+    } else {
+        setVisibleSlide(state.currentSlideIndex);
+    }
+}
+
+function backwardSlide() {
+    if (state.currentSlideIndex > 0) {
+        setVisibleSlide(state.currentSlideIndex - 1);
+    } else {
+        setVisibleSlide(state.currentSlideIndex);
+    }
+}
+
+function animateTransition(active) {
+    if (active) {
+        slider.style.transition = 'transform .3s';
+    } else {
+        slider.style.removeProperty('transition');
+    }
+}
+
+function activeControlButton(index) {
+    btnControls.forEach(function(item) {
+        item.classList.remove('active')
+    });
+    const btnControl = btnControls[index];
+    btnControl.classList.add('active');
+}
+
+function activeImageTitle(index) {
+    imgTitles.forEach(function(item) {
+        item.classList.remove('active')
+    });
+    const imgTitle = imgTitles[index];
+    imgTitle.classList.add('active');
+}
+
+function setVisibleSlide(index) {
+    state.currentSlideIndex = index;
+    activeControlButton(index);
+    activeImageTitle(index);
+    const position = getCenterPosition(index);
+    animateTransition(true);
+    translateSlide(position);
+}
+
 function preventDefault(e) {
     e.preventDefault();
 }
 
-function onMouseDown(event) {
+function onControlButtonClick(index) {
+    setVisibleSlide(index);
+}
+
+function onMouseDown(event, index) {
     const slide = event.currentTarget;
     slide.addEventListener('mousemove', onMouseMove);
     state.mouseDownPosition = event.clientX;
     state.currentSliderPosition = event.clientX - state.lastTranslatePosition;
+    state.currentSlideIndex = index;
+    animateTransition(false);
 }
 
 function onMouseUp(event) {
     const slide = event.currentTarget;
+    if(state.movementPosition > 150) {
+        backwardSlide();
+    } else if (state.movementPosition < -150) {
+        forwardSlide();
+    } else {
+        const calc = getCenterPosition(state.currentSlideIndex);
+        translateSlide(calc);
+    }
     slide.removeEventListener('mousemove', onMouseMove);
 }
 
 function onMouseMove(event) {
+    state.movementPosition = event.clientX - state.mouseDownPosition;
     translateSlide(event.clientX - state.currentSliderPosition);
-    console.log(event.clientX)
 }
 
 function onMouseLeave(event) {
     const slide = event.currentTarget;
     slide.removeEventListener('mousemove', onMouseMove)
-    console.log('tirou o mouse de cima do elemento');
 }
 
 function setListeners() {
+    btnNext.addEventListener('click', forwardSlide);
+    btnPrevious.addEventListener('click', backwardSlide);
     sliderItems.forEach(function(slide, index) {
         const link = slide.querySelector('a.banner-slider__link');
         link.addEventListener('click', preventDefault);
         slide.addEventListener('dragstart', preventDefault);
-        slide.addEventListener('mousedown', onMouseDown);
+        slide.addEventListener('mousedown', function(event) {
+            onMouseDown(event, index);
+        });
         slide.addEventListener('mouseup', onMouseUp);
         slide.addEventListener('mouseleave', onMouseLeave);
+        btnControls[index].addEventListener('click', function(event) {
+            onControlButtonClick(index)
+        });
     })
 }
 
 function init() {
+    setVisibleSlide(0);
     setListeners();
 }
 
