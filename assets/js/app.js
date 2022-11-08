e24a6980e23aee8c12f5c0d4b415118acfa76828
Toshio Yasunaka <toshioyasunaka@hotmail.com>
2022-11-08 19:25:45 -0300

    New: dynamic content and fetch API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,9 @@
 import { getHomeContent } from "./services/getHomeContent.js";
+import Home from './pages/home.js';
 
 getHomeContent()
     .then((data) => {
-        console.log('app.js', data)
+        Home(data);
     })
     .catch((error) => {
         console.log(error);
--- assets/js/modules/header.js
+++ assets/js/modules/header.js
@@ -1,37 +1,41 @@
-const header = document.querySelector('[data-header]'); //select the first attribute data-header;
-const openNavSubmenu = document.querySelector('[data-open-navsubmenu]');
-const navSubmenu = document.querySelector('[data-navsubmenu]');
-const usermenu = document.querySelector('[data-usermenu]');
-const openUsermenu = document.querySelector('[data-open-usermenu]');
-
-function onWindowScroll() {
-    if (window.scrollY > 20) {
-        header.style.backgroundColor = '#0C0D14';
-    } else {
-        header.style.backgroundColor = 'transparent';
+const headerModule = () => {
+    const header = document.querySelector('[data-header]'); //select the first attribute data-header;
+    const openNavSubmenu = document.querySelector('[data-open-navsubmenu]');
+    const navSubmenu = document.querySelector('[data-navsubmenu]');
+    const usermenu = document.querySelector('[data-usermenu]');
+    const openUsermenu = document.querySelector('[data-open-usermenu]');
+    
+    function onWindowScroll() {
+        if (window.scrollY > 20) {
+            header.style.backgroundColor = '#0C0D14';
+        } else {
+            header.style.backgroundColor = 'transparent';
+        }
+    }
+    
+    function onTouchOpenNavSubmenu(e) {
+        e.preventDefault();
+        navSubmenu.classList.toggle('active');
+    }
+    
+    function onTouchOpenUsermenu(e) {
+        e.preventDefault();
+        usermenu.classList.toggle('active');
+    }
+    
+    function setListeners() {
+        window.addEventListener('scroll', onWindowScroll)
+        openNavSubmenu.addEventListener('touchstart', onTouchOpenNavSubmenu)
+        openUsermenu.addEventListener('touchstart', onTouchOpenUsermenu)
+    }
+    
+    function init() {
+        setListeners();
+    }
+    
+    return {
+        init
     }
 }
 
-function onTouchOpenNavSubmenu(e) {
-    e.preventDefault();
-    navSubmenu.classList.toggle('active');
-}
-
-function onTouchOpenUsermenu(e) {
-    e.preventDefault();
-    usermenu.classList.toggle('active');
-}
-
-function setListeners() {
-    window.addEventListener('scroll', onWindowScroll)
-    openNavSubmenu.addEventListener('touchstart', onTouchOpenNavSubmenu)
-    openUsermenu.addEventListener('touchstart', onTouchOpenUsermenu)
-}
-
-function init() {
-    setListeners();
-}
-
-export default {
-    init
-}
+export default headerModule
